fb3efcd42dcf5a06d7103054fc7ff4704edfff78
Prasad <thakareprasad28@yahoo.in>
2022-01-04 19:52:39 +0530

    added styles

--- client/src/global/Navbar.js
+++ client/src/global/Navbar.js
@@ -1,5 +1,6 @@
 import { Link, useHistory } from "react-router-dom";
 import axios from 'axios';
+import '../styles/Navbar.css';
 
 const Navbar = (props) => {
 
@@ -20,41 +21,17 @@ const Navbar = (props) => {
         <Link to="/"><h1>React Blogs</h1></Link>
         <div className="links">
 
-        {props.user && <Link to="/dashboard">Dashboard</Link>}
+          {props.user && <Link to="/dashboard">Dashboard</Link>}
 
-          {!props.user && <Link to="/signup" style={{
-            color : "white",
-            backgroundColor : "#f1356d",
-            borderRadius : "8px"
-          }}>Sign Up</Link>}
+          {!props.user && <Link to="/signup" className="navbar-component">Sign Up</Link>}
 
-          {!props.user && <Link to="/login" style={{
-            color : "white",
-            backgroundColor : "#f1356d",
-            borderRadius : "8px"
-          }}>Login</Link>}
+          {!props.user && <Link to="/login" className="navbar-component">Login</Link>}
 
-            {props.user && <Link to="/search" style={{
-            color : "white",
-            backgroundColor : "#f1356d",
-            borderRadius : "8px"
-          }}><span>&#x1F50E;</span></Link>}
+          {props.user && <Link to="/search" className="navbar-component"><span>&#x1F50E;</span></Link>}
 
-          {props.user && <Link to="/create" style={{
-            color : "white",
-            backgroundColor : "#f1356d",
-            borderRadius : "8px"
-          }}>New Blog</Link>}
+          {props.user && <Link to="/create" className="navbar-component">New Blog</Link>}
 
-          {props.user && <button onClick={handleLogout} style={{
-            color : "white",
-            backgroundColor : "#f1356d",
-            borderRadius : "8px",
-            borderWidth : "0px",
-            padding : "6px",
-            marginLeft : "16px",
-            fontSize : "16px"
-          }}>Logout</button>}
+          {props.user && <button onClick={handleLogout} className="navbar-component">Logout</button>}
 
         </div>
       </nav>
